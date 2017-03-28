bb4b0337ad298b8e2ab48e68024f2cce8062372b
oswaldocruzsimon <oswaldo_cruz@javeriana.edu.co>
2017-03-28 17:49:42 -0600

    se creo un consumer

---
+++ consumer.py
@@ -0,0 +1,10 @@
+from kafka import KafkaConsumer
+consumer = KafkaConsumer(bootstrap_servers='localhost:9092',auto_offset_reset='earliest')
+consumer.subscribe(['test'])
+
+for message in consumer:
+	print(message.value)
+
+
+
+
